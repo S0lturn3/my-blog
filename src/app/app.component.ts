4d325749d2eb1b6016e385abeb45b716bc0cbc5d
ERICK  Carvalho Paulette de Oliveira <ecoliveira@sispro.com.br>
2024-03-11 17:22:30 -0300

    resolving other conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { HeaderComponent } from './core/components/header/header.component';
-=======
->>>>>>> 557b68b8445d2fe3d769a703d636e85a1142bd7a
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
   imports: [
     HeaderComponent,
     
     CommonModule,
     RouterOutlet
   ],
-=======
-  imports: [CommonModule, RouterOutlet],
->>>>>>> 557b68b8445d2fe3d769a703d636e85a1142bd7a
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
